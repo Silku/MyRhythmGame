f17bd8318c41aac7cfabf09312fbd84178b89e03
김태우 <kokicola@naver.com>
2022-11-09 20:06:06 +0900

    221109 오디오 테스트

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-(function() {
+/**
+ * Todo
+ * ui
+ * 충돌 판정 : perfect ~ miss
+ * 배속
+ * 
+ */
+
+
     let canvas = document.querySelector('#canvas');
     let ctx = canvas.getContext('2d');
 
-    canvas.width = window.innerWidth - 100;
-    canvas.height = window.innerHeight - 100;
-
+    canvas.width = 1080;
+    canvas.height = 720;
     displayWidth = canvas.width;
+    // displayHeight = canvas.height;
 
-    // let note = {
-    //     x : 10,
-    //     y: 100,
-    //     width : 100,
-    //     height : 30,
-    //     draw(){
-    //         ctx.fillStyle = 'rgb(0,255,0)'
-    //         ctx.fillRect(this.x,this.y,this.width,this.height)
-    //     }
-    // }
+    let background = {
+        draw(){
+            ctx.fillStyle = 'rgb(0,0,0)';
+            ctx.fillRect(0,0 ,1080,720);
+        }
+    }
 
-    // note.draw();
-
+    // 판정선
     let judgeLine = {
-        x : 10,
-        y: 700,
+        x : 0,
+        y: 670,
         width : displayWidth,
         height : 10,
         draw(){
@@ -31,9 +34,11 @@
         }
     }
 
+
+    // 324, 432, 540 ,648, 756
     class Note{
         constructor(){
-            this.x = 300;
+            this.x = 432;
             this.y = 100;
             this.width = 100;
             this.height = 30;
@@ -45,41 +50,62 @@
     }
 
 
+    let su = Math.ceil(Math.random()*10)
+    console.log(su)
 
     let time = 0;
     let notes = [];
     // 노트 애니메이션
+
     function dropNote(){
+        background.draw();
+        judgeLine.draw();
         requestAnimationFrame(dropNote)
         time++;
-        ctx.clearRect(0,0, canvas.width, canvas.height)
+        // time +=  Math.ceil(Math.random()*10);
+        // ctx.clearRect(0,0, canvas.width, canvas.height)
         if(time % 120 === 0){
             let note = new Note();
             let note2 = new Note();
-            note2.x = 500;
+            note2.x = 540;
             notes.push(note, note2);
         }
-        notes.forEach((e)=>{
+        notes.forEach((e , i , o)=>{
             e.y++;
+            if(e.y > 670){
+                o.splice(i, 1)
+            }
             e.draw();
         })
-        judgeLine.draw();
+
+
     }
-
     dropNote();
-})()
 
 
 
-// function draw() {
-//     var ctx = document.getElementById('canvas').getContext('2d');
-//     for (var i = 0; i < 6; i++){
-//       for (var j = 0; j < 6; j++){
-//         ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ', ' +
-//                          Math.floor(255 - 42.5 * j) + ', 0)';
-//         ctx.fillRect(j*25,i*25,25,25);
-//       }
-//     }
-//   }
+    document.addEventListener('keydown',function(e){
+        if(e.key === 'f'){
+            console.log('f누름')
+        }
+    })
 
-//   draw()
+    document.addEventListener('keydown',function(e){
+        if(e.key === 'q'){
+            console.log('f누름')
+            audio.pause();
+            audio.currentTime = 0;
+        }
+    })
+
+
+
+
+const mp3 = document.querySelector('#mp3')
+const audio = new Audio;
+audio.src='./assets/only for you.mp3'
+mp3.addEventListener('click', function(){
+        audio.play();
+        audio.volume = 0.1
+
+})
